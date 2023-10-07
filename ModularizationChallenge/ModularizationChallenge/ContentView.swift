825504b00e4fe9a6ec671d134c05329f8ac7b34c
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2023-10-07 11:01:15 -0300

    initial files

---
+++ ModularizationChallenge/ModularizationChallenge/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ModularizationChallenge
+//
+//  Created by Luiz Araujo on 07/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ModularizationChallenge/ModularizationChallenge/ModularizationChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModularizationChallengeApp.swift
+//  ModularizationChallenge
+//
+//  Created by Luiz Araujo on 07/10/23.
+//
+
+import SwiftUI
+
+@main
+struct ModularizationChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
